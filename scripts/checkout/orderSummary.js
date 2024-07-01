3fa0ab112f65ccb898c5d0fc8deb2548b6bc6ba5
Sulabh <sulabhambule@gmail.com>
2024-07-01 13:00:42 +0530

    Move OrderSummary into its own file

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,7 +2,7 @@ import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
 import { products } from "../../data/products.js"; // Named Export
 import { formatCurrency } from "../utils/money.js";
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; // default export.
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; // defaut export.
 import {deliveryOptions} from '../../data/deliveryOption.js';
 
 
