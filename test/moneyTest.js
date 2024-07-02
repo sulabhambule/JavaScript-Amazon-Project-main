f42b42b541923bc02068cfbca11ee3cfd426b8c4
Sulabh <sulabhambule@gmail.com>
2024-07-02 10:52:51 +0530

    Add Jasmine Testing FrameWork

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,26 +1,26 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import { formatCurrency } from "../scripts/utils/money.js";
 
-console.log('Test suite: formatCurrency');
+console.log("Test suite: formatCurrency");
 
-console.log('Convert cents ino dollar');
-if(formatCurrency(2095) === '20.95') {
-  console.log('passed');
+console.log("Convert cents ino dollar");
+if (formatCurrency(2095) === "20.95") {
+  console.log("passed");
 } else {
-  console.log('failed');
+  console.log("failed");
 }
 
-console.log('works with Zero');
+console.log("works with Zero");
 
-if (formatCurrency(0) === '0.00') {
-  console.log('passed');
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
 } else {
-  console.log('Failed');
+  console.log("Failed");
 }
 
-console.log('Round up to the nearest cent');
+console.log("Round up to the nearest cent");
 
-if (formatCurrency(2000.5) === '20.01') {
-  console.log('passed');
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
 } else {
-  console.log('Failed');
+  console.log("Failed");
 }
