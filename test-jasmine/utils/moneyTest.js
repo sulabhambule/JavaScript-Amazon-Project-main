26aeebddae6f4a881f7fcf996d83814a77a9d0ba
Sulabh <sulabhambule@gmail.com>
2024-07-02 22:35:11 +0530

    Create  automated Test for AddToCart

--- test-jasmine/utils/moneyTest.js
+++ test-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 describe('Test suite: formatCurrency', () => {
   it('Convert cents ino dollar', () => {
